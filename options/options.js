26496cc5d2918093f30137a712d270cdfdc1bd92
Crowdbotics <team@crowdbotics.com>
2024-02-05 19:24:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aicompliancemonitor-45973",
+  url: "https://aicompliancemonitor-45973.botics.co",
+  api: "https://aicompliancemonitor-45973.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
